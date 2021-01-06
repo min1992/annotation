db8677bc5508dbeb94d94ad187bacdcd7e11847f
min1992 <38031850+min1992@users.noreply.github.com>
2021-01-06 15:06:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /biocluster/data/biobk/user_test/chenhuimin/20.other/bin/main.py --refseq /biocluster/data/biobk/user_test/chenhuimin/20.other/00.ref/refseq_addEnsemble.sorted.txt --variant $1 --reference /biocluster/data/bioexec/database/genome/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta --spliceSize 2
+python main.py --refseq refseq_addEnsemble.sorted.txt --variant $1 --reference Homo_sapiens_assembly19.fasta --spliceSize 2
